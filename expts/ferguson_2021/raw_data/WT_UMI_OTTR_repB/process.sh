6a72a3b6c514b51f48fb99836627aa0f3aa2ada6
Amanda Mok <a.mok@berkeley.edu>
2021-12-05 13:43:44 -0800

    filter reads by length before alignment steps

--- expts/ferguson_2021/raw_data/WT_UMI_OTTR_repB/process.sh
+++ expts/ferguson_2021/raw_data/WT_UMI_OTTR_repB/process.sh
@@ -4,7 +4,7 @@
 # umi_tools version 1.0.0
 # bowtie version 1.0.0
 # samtools version 1.7
-## note: unset PYTHONPATH before running cutadapt 
+## note: unset PYTHONPATH before running cutadapt
 
 ref_dir="$HOME/footprint-bias/reference_data"
 unset PYTHONPATH
@@ -40,21 +40,30 @@ ottrUMI_trimmed_adaptor_umi_nta_tprt.cutadapt
 ## T: 13 169 (0.0%)
 ## N: 192 (0.0%)
 
+# 3. filter reads by length
+cutadapt -m 27 -M 31 -j 10 -o ottrUMI_trimmed_adaptor_umi_nta_tprt_25to29.fq \
+ottrUMI_trimmed_adaptor_umi_nta_tprt.fq > \
+ottrUMI_trimmed_adaptor_umi_nta_tprt_25to29.cutadapt
+## 51 474 631 input reads
+## 27 979 235 (54.4%) reads too short
+## 9 616 407 (18.7%) reads too long
+## 13 878 989 (27.0%) reads written
+
 # 5. remove contaminant reads: rRNA
 bowtie -v 2 -p 10 -S --un ottrUMI_trimmed_not_rrna.fq \
-$ref_dir/ScerRRNA ottrUMI_trimmed_adaptor_umi_nta_tprt.fq > \
+$ref_dir/ScerRRNA ottrUMI_trimmed_adaptor_umi_nta_tprt_25to29.fq > \
 ottrUMI_trimmed_rrna.sam 2> ottrUMI_trimmed_rrna.bowtiestats
-## 51 474 631 input reads
-## 41 975 544 (81.55%) reads with ≥1 alignment
-## 9 499 087 (18.45%) unaligned reads
+## 13 878 989 input reads
+## 9 692 641 (69.84%) reads with ≥1 alignment
+## 4 186 348 (30.16%) unaligned reads
 
 # 6. remove contaminant reads: ncRNA
 bowtie -v 2 -p 10 -S --un ottrUMI_trimmed_not_rrna_ncrna.fq \
 $ref_dir/rna_coding ottrUMI_trimmed_not_rrna.fq > \
 ottrUMI_trimmed_ncrna.sam 2> ottrUMI_trimmed_ncrna.bowtiestats
-## 9 499 087 input reads
-## 86 385 (0.91%) reads with ≥1 alignment
-## 9 412 702 (99.09%) unaligned reads
+## 4 186 348 input reads
+## 36 350 (0.87%) reads with ≥1 alignment
+## 4 149 998 (99.13%) unaligned reads
 
 # 7. deduplicate: report all best alignments (-a --best --strata)
 bowtie -v 2 -p 10 -S --norc -a --best --strata \
@@ -63,9 +72,9 @@ $ref_dir/scer.transcripts.20cds20 \
 ottrUMI_trimmed_not_rrna_ncrna.fq > \
 ottrUMI_trimmed_not_rrna_ncrna_best.sam 2> \
 ottrUMI_trimmed_not_rrna_ncrna_best.bowtiestats
-## 7 584 415 (80.58%) reads with ≥1 alignment
-## 1 828 287 (19.42%) unaligned reads
-## 11 027 228 reported alignments
+## 3 933 044 (94.77%) reads with ≥1 alignment
+## 216 954 (5.23%) unaligned reads
+## 5 679 106 reported alignments
 
 # 8. deduplicate: sort by read name
 samtools view -h -F 0x04 ottrUMI_trimmed_not_rrna_ncrna_best.sam | \
@@ -87,9 +96,9 @@ umi_tools dedup --output-stats=deduplicated --out-sam --read-length \
 -S ottrUMI_trimmed_deduplicated.sam \
 -L ottrUMI_trimmed_deduplicated.umitools \
 -E ottrUMI_trimmed_deduplicated.error
-## 7 584 415 reads in
-## 1 973 980 positions deduplicated
-## 4 515 446 reads out
+## 3 933 044 reads in
+## 808 567 positions deduplicated
+## 2 283 876 reads out
 
 # 12. deduplicate: convert into fastq
 samtools fastq ottrUMI_trimmed_deduplicated.sam > ottrUMI_trimmed_deduplicated.fq
@@ -99,14 +108,13 @@ bowtie -v 2 -p 10 -S --norc -a $ref_dir/scer.transcripts.20cds20 \
 ottrUMI_trimmed_deduplicated.fq > \
 ottrUMI_trimmed_deduplicated_footprints.sam 2> \
 ottrUMI_trimmed_deduplicated_footprints.bowtiestats
-## 4 515 446 unique reads
-## 7 349 578 alignments
+## 2 283 876 unique reads
+## 3 620 934 alignments
 
 # 14. compute multimapping weights
-rsem-calculate-expression --seed-length 20 --sam \
+rsem-calculate-expression --sam \
 ottrUMI_trimmed_deduplicated_footprints.sam \
 $ref_dir/scer.transcripts.20cds20 \
 ottrUMI_trimmed_deduplicated_footprints > \
 ottrUMI_trimmed_deduplicated_footprints.rsem.stdout 2> \
 ottrUMI_trimmed_deduplicated_footprints.rsem.stderr
-
--- expts/ferguson_2021/raw_data/mcglincy_repB/process.sh
+++ expts/ferguson_2021/raw_data/mcglincy_repB/process.sh
@@ -4,7 +4,7 @@
 # umi_tools version 1.0.0
 # bowtie version 1.0.0
 # samtools version 1.7
-## note: unset PYTHONPATH before running cutadapt 
+## note: unset PYTHONPATH before running cutadapt
 
 ref_dir="$HOME/footprint-bias/reference_data"
 unset PYTHONPATH
@@ -14,6 +14,7 @@ cat McGlincy_round1.fastq.gz McGlincy_round2.fastq.gz | zcat | \
 cutadapt -a CGTAAAGATCGGAAGAGCAC -j 10 -m 20 \
 --untrimmed-output mcglincy_untrimmed.fq \
 -o mcglincy_trimmed_adaptor.fq - > mcglincy_trimmed_adaptor.cutadapt
+##
 
 # 2. preprocess: extract 2nt from 5' end and 5nt from 3' end as UMI; trim
 umi_tools extract --extract-method=regex \
@@ -22,74 +23,81 @@ umi_tools extract --extract-method=regex \
 -S mcglincy_trimmed_adaptor_umi.fq \
 -L mcglincy_trimmed_adaptor_umi.umitools
 
-# 3. remove contaminant reads: rRNA
-bowtie -v 2 -p 10 -S --un mcglincy_trimmed_not_rrna.fq \
-$ref_dir/ScerRRNA mcglincy_trimmed_adaptor_umi.fq > \
-mcglincy_trimmed_rrna.sam 2> mcglincy_trimmed_rrna.bowtiestats
+# 3. filter reads by length
+cutadapt -m 27 -M 31 -j 10 -o mcglincy_trimmed_adaptor_umi_27to31.fq \
+mcglincy_trimmed_adaptor_umi.fq > mcglincy_trimmed_adaptor_umi_27to31.cutadapt
 ## 78 342 763 input reads
-## 59 351 550 (75.76%) reads with ≥1 alignment
-## 18 991 213 (24.24%) unaligned reads
+## 55 621 518 (71.0%) reads too short
+## 8 881 388 (11.3%) reads too long
+## 13 839 857 (17.7%) reads written
 
-# 4. remove contaminant reads: ncRNA
+# 4. remove contaminant reads: rRNA
+bowtie -v 2 -p 10 -S --un mcglincy_trimmed_not_rrna.fq \
+$ref_dir/ScerRRNA mcglincy_trimmed_adaptor_umi_27to31.fq > \
+mcglincy_trimmed_rrna.sam 2> mcglincy_trimmed_rrna.bowtiestats
+## 13 839 857 input reads
+## 7 056 871 (50.99%) reads with ≥1 alignment
+## 6 782 986 (49.01%) unaligned reads
+
+# 5. remove contaminant reads: ncRNA
 bowtie -v 2 -p 10 -S --un mcglincy_trimmed_not_rrna_ncrna.fq \
 $ref_dir/rna_coding mcglincy_trimmed_not_rrna.fq > \
 mcglincy_trimmed_ncrna.sam 2> mcglincy_trimmed_ncrna.bowtiestats
-## 18 991 213 input reads
-## 1 579 426 (8.32%) reads with ≥1 alignment
-## 17 411 787 (91.68%) unaligned reads
+## 6 782 986 input reads
+## 263 135 (3.88%) reads with ≥1 alignment
+## 6 519 851 (96.12%) unaligned reads
 
-# 5. deduplicate: report all best alignments (-a --best --strata)
+# 6. deduplicate: report all best alignments (-a --best --strata)
 bowtie -v 2 -p 10 -S --norc -a --best --strata \
 --un mcglincy_trimmed_unaligned.fq \
 $ref_dir/scer.transcripts.20cds20 \
 mcglincy_trimmed_not_rrna_ncrna.fq > \
 mcglincy_trimmed_not_rrna_ncrna_best.sam 2> \
 mcglincy_trimmed_not_rrna_ncrna_best.bowtiestats
-## 17 411 787 input reads
-## 8 408 263 (48.29%) reads with ≥1 alignment
-## 9 003 524 (51.71%) unaligned reads
-## 16 642 462 reported alignments
+## 6 519 851 input reads
+## 4 365 636 (66.96%) reads with ≥1 alignment
+## 2 154 215 (33.04%) unaligned reads
+## 6 187 804 reported alignments
 
-# 6. deduplicate: sort by read name
+# 7. deduplicate: sort by read name
 samtools view -h -F 0x04 mcglincy_trimmed_not_rrna_ncrna_best.sam | \
 samtools sort -n -O SAM -o mcglincy_trimmed_not_rrna_ncrna_best_sorted.sam
 
-# 7. deduplicate: choose positionally-first alignment per read
+# 8. deduplicate: choose positionally-first alignment per read
 Rscript $HOME/footprint-bias/scripts/filter_sam.R \
 -i mcglincy_trimmed_not_rrna_ncrna_best_sorted.sam \
 -o mcglincy_trimmed_not_rrna_ncrna_best_sorted_filtered.sam
 
-# 8. deduplicate: sort and index
+# 9. deduplicate: sort and index
 samtools sort mcglincy_trimmed_not_rrna_ncrna_best_sorted_filtered.sam \
 -o mcglincy_trimmed_not_rrna_ncrna_unique.bam -O BAM
 samtools index mcglincy_trimmed_not_rrna_ncrna_unique.bam
 
-# 9. deduplicate: deduplicate UMIs
+# 10. deduplicate: deduplicate UMIs
 umi_tools dedup --output-stats=deduplicated --in-sam --out-sam --read-length \
 -I mcglincy_trimmed_not_rrna_ncrna_unique.bam \
 -S mcglincy_trimmed_deduplicated.sam \
 -L mcglincy_trimmed_deduplicated.umitools \
 -E mcglincy_trimmed_deduplicated.error
-## 8 408 263 reads in
-## 1 940 063 positions deduplicated
-## 3 906 538 reads out
+## 4 365 636 reads in
+## 801 550 positions deduplicated
+## 1 966 512 reads out
 
-# 10. deduplicate: convert into fastq
+# 11. deduplicate: convert into fastq
 samtools fastq mcglincy_trimmed_deduplicated.sam > mcglincy_trimmed_deduplicated.fq
 
-# 11. align to transcriptome
+# 12. align to transcriptome
 bowtie -v 2 -p 10 -S --norc -a $ref_dir/scer.transcripts.20cds20 \
 mcglincy_trimmed_deduplicated.fq > \
 mcglincy_trimmed_deduplicated_footprints.sam 2> \
 mcglincy_trimmed_deduplicated_footprints.bowtiestats
-## 3 906 538 unique reads
-## 27 819 829 alignments
+## 1 966 512 unique reads
+## 3 070 093 alignments
 
-# 12. compute multimapping weights
-rsem-calculate-expression --seed-length 15 --sam \
+# 13. compute multimapping weights
+rsem-calculate-expression --sam \
 mcglincy_trimmed_deduplicated_footprints.sam \
 $ref_dir/scer.transcripts.20cds20 \
 mcglincy_trimmed_deduplicated_footprints > \
 mcglincy_trimmed_deduplicated_footprints.rsem.stdout 2> \
 mcglincy_trimmed_deduplicated_footprints.rsem.stderr
-
